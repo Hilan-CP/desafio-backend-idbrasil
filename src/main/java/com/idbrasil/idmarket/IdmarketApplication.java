9cf20554cd70b12de7c5ec4a99f9e92c8842b025
Hilan <hilan.paganini@gmail.com>
2025-09-10 19:41:46 -0300

    Projeto criado

---
+++ src/main/java/com/idbrasil/idmarket/IdmarketApplication.java
@@ -0,0 +1,13 @@
+package com.idbrasil.idmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idbrasil/idmarket/IdmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idbrasil.idmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
